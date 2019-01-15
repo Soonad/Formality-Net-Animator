7ee311a013ac968b9e03cf1d71f0b03345ace823
Maisa <maisa.milena@gmail.com>
2019-01-15 11:37:00 -0200

    making all lines

--- index.js
+++ index.js
@@ -49,22 +49,26 @@ var initialNode = new InitialNode({x: width * 0.47, y: height * 0.05}, 5)
 var node0 = new Node(0, {x: width * 0.5, y: height * 0.2}, getRadianFromAngle(90));
 nodes.push(node0);
 
+
 var node1 = new Node(0, {x: width * 0.3, y: height * 0.40}, getRadianFromAngle());
 nodes.push(node1);
 
-var node2 = new Node(0, {x: width - (width * 0.3), y: height * 0.40}, getRadianFromAngle());
+var node2 = new Node(1, {x: width * 0.20, y: height * 0.60}, getRadianFromAngle());
 nodes.push(node2);
 
-var node3 = new Node(1, {x: width * 0.20, y: height * 0.60}, getRadianFromAngle());
-nodes.push(node3);
+// -10 is to align an upside down node with the others
+var node3 = new Node(0, {x: width * 0.40, y: height * 0.60 - 10}, getRadianFromAngle(90));
+nodes.push(node3); 
 
-var node4 = new Node(0, {x: width * 0.40, y: height * 0.60}, getRadianFromAngle(90));
-nodes.push(node4); 
 
+var node4 = new Node(0, {x: width - (width * 0.3), y: height * 0.40}, getRadianFromAngle());
+nodes.push(node4);
 
 window.onload = function(){
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
+
+    // context.clearRect(0, 0, canvas.width, canvas.height);
 
     setInitialNode(context);
 
@@ -75,7 +79,8 @@ window.onload = function(){
         for (node of nodes) {    
             
             drawTriangle(context, node); 
-            setLines(context); 
+            
+            // OBS: The node painted is the next one, not the real type 1
             if (node.type == 1) {
                 context.strokeStyle = 'blue';
             } else {
@@ -96,7 +101,8 @@ function setInitialNode(context) {
 function drawTriangle(context, node) {
     context.beginPath();
     // Port 0 to 1
-    context.moveTo(node.getPortPosition(0).x, node.getPortPosition(0).y);
+    context.moveTo(node
+        .getPortPosition(0).x, node.getPortPosition(0).y);
     context.lineTo(node.getPortPosition(1).x, node.getPortPosition(1).y);
     
     // Port 1 to 2
@@ -114,7 +120,8 @@ function drawTriangle(context, node) {
 function setLines(context) {
     // bezierCurveTo: 
         // Control point 1 (first two numbers), Control point 2 (second two numbers) and end point (last two numbers)
-
+    // context.strokeStyle = 'black'; 
+    
     // Initial node to Node 0 (1)
     context.beginPath();
     context.moveTo(initialNode.position.x, initialNode.position.y);
@@ -132,9 +139,41 @@ function setLines(context) {
     // Node 1 (1) to Node 2 (0)   
     context.beginPath();
     context.moveTo(nodes[1].getPortPosition(1).x, nodes[1].getPortPosition(1).y);
-    context.bezierCurveTo(nodes[1].position.x, nodes[1].position.y + 30, 
-                          nodes[2].position.x, nodes[1].position.y - 30, 
+    context.bezierCurveTo(nodes[1].getPortPosition(1).x, nodes[1].getPortPosition(1).y + 30, 
+                          nodes[2].position.x, nodes[2].position.y - 30, 
                           nodes[2].getPortPosition(0).x, nodes[2].getPortPosition(0).y);
     context.stroke();
+
+    // Node 1 (2) to Node 3 (0)   
+    context.beginPath();
+    context.moveTo(nodes[1].getPortPosition(2).x, nodes[1].getPortPosition(2).y);
+    context.bezierCurveTo(nodes[1].getPortPosition(2).x - 10, nodes[1].getPortPosition(2).y + 30,  
+                          nodes[3].getPortPosition(2).x, nodes[3].getPortPosition(2).y - 10, 
+                          nodes[3].getPortPosition(2).x, nodes[3].getPortPosition(2).y);
+    context.stroke();
+
+    // Node 0 (1) to Node 4 (0)   
+    context.beginPath();
+    context.moveTo(nodes[0].getPortPosition(1).x, nodes[0].getPortPosition(1).y);
+    context.bezierCurveTo(nodes[0].getPortPosition(1).x, nodes[0].getPortPosition(1).y - 50,  
+                          nodes[0].getPortPosition(1).x + 60, nodes[0].getPortPosition(1).y - 50, 
+                          nodes[4].getPortPosition(0).x, nodes[4].getPortPosition(0).y);
+    context.stroke();
+
+    // Node 2 (1) to Node 3 (0)   
+    context.beginPath();
+    context.moveTo(nodes[2].getPortPosition(1).x, nodes[2].getPortPosition(1).y);
+    context.bezierCurveTo(nodes[2].getPortPosition(1).x, nodes[2].getPortPosition(1).y + 50,  
+                          nodes[3].getPortPosition(0).x, nodes[3].getPortPosition(0).y + 50, 
+                          nodes[3].getPortPosition(0).x, nodes[3].getPortPosition(0).y);
+    context.stroke();    
+
+    // Node 2 (2) to Node 3 (1)   
+    context.beginPath();
+    context.moveTo(nodes[2].getPortPosition(2).x, nodes[2].getPortPosition(2).y);
+    context.bezierCurveTo(nodes[2].getPortPosition(2).x, nodes[2].getPortPosition(2).y + 70,  
+                          nodes[3].getPortPosition(1).x + 30, nodes[3].getPortPosition(1).y + 70, 
+                          nodes[3].getPortPosition(1).x, nodes[3].getPortPosition(1).y);
+    context.stroke(); 
 
 }
