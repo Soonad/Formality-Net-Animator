0cccaa37a077a7977303831272ac259ab05f846a
Maisa <maisa.milena@gmail.com>
2019-02-04 20:30:25 -0200

    refactoring

--- index.js
+++ index.js
@@ -27,15 +27,19 @@ const width = 400;
  * Type pivot: ["pivot", node, port] 
  */
 var elementSelected = null; 
-// A node that will change it's angle
-// Type node: ["node", node]
+// A node that will change it's angle. Type node: ["node", node]
 var elementClicked = null;
+
+// -- Keys auxiliars --
+var animate = null;
+var ctrlPressed = false;
+var hidePivots = false;
+
 var prevPositionMovement = []; // [{x: 0, y: 0}] Adds all previous positions for elements moving. Does not identifies which objects move, only the position
 
 var selectionColor = 'green';
 var nodeIdCounter = 0;
 
-var animate = null;
 
 // Nodes
 // var initialNode = new Node(0, {x: width * 0.47 - 5, y: height * 0.05}, getRadianFromAngle());
@@ -44,6 +48,47 @@ var nodes = makeNodes();
 
 // An Node array recording the changes of the positions 
 var keyframes = []; // [[node0, node1, node2..], [node0, node1, node2...] ...]
+
+
+// Defines the properties of each node
+function makeNodes() {
+    var nodes = [];
+    var initialNode = new Node(0, {x: width * 0.47 - 5, y: height * 0.05}, getRadianFromAngle()); 
+    var node0 = new Node(1, {x: width * 0.5, y: height * 0.2}, getRadianFromAngle(90));
+    var node1 = new Node(1, {x: width * 0.3, y: height * 0.40}, getRadianFromAngle());
+    var node2 = new Node(2, {x: width * 0.20, y: height * 0.60}, getRadianFromAngle());
+    // -10 is to align an upside down node with the others
+    var node3 = new Node(1, {x: width * 0.40, y: height * 0.60 - 10}, getRadianFromAngle(90));
+    var node4 = new Node(1, {x: width - (width * 0.3), y: height * 0.40}, getRadianFromAngle());
+    
+    nodes.push(initialNode);
+    nodes.push(node0);
+    nodes.push(node1);
+    nodes.push(node2);
+    nodes.push(node3); 
+    nodes.push(node4);
+    
+    for (var i = 0; i < nodes.length; i++) {
+        nodes[i].id = nodeIdCounter++;
+    }
+
+    // Connections between ports
+    connectPorts([node0, 0], [node1, 0]);
+    connectPorts([node0, 1], [node4, 0]);
+    connectPorts([node0, 2],[initialNode, 0]);
+    connectPorts([initialNode, 1], [initialNode, 2]);
+
+    connectPorts([node1, 1], [node2, 0]);
+    connectPorts([node1, 2], [node3, 2]);
+
+    connectPorts([node2, 1], [node3, 0]);
+    connectPorts([node2, 2], [node3, 1]);
+
+    connectPorts([node4, 1], [node4, 2]);
+
+    return nodes;
+}
+
 
 window.onload = function() {
     var canvas = document.getElementById("canvas");
@@ -56,30 +101,7 @@ window.onload = function() {
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         if (animate) {
-            var animKeyframe = (currentKeyframe + ((Date.now() - animate) / 1000)) % keyframes.length;
-            var nodesA = keyframes[Math.floor(animKeyframe)];
-            var nodesB = keyframes[Math.floor(animKeyframe + 1) % keyframes.length];
-            var animNodes = copyNodes(nodesA);
-
-            // There was no reduction or duplication
-            if (nodesA.length === nodesB.length) {
-                // updates position, angle and pivots of a node
-                for (var i = 0; i < animNodes.length; ++i) {
-                    var t = animKeyframe % 1;
-                    var {x: ax, y: ay} = nodesA[i].position;
-                    var {x: bx, y: by} = nodesB[i].position;
-                    var aa = nodesA[i].angle;
-                    var ba = nodesB[i].angle;
-                    animNodes[i].position = {x: ax+(bx-ax)*t, y: ay+(by-ay)*t};
-                    animNodes[i].angle = aa + (ba - aa) * t;
-
-                    for (var j = 0; j < 3; j++) {
-                        var {x: pax, y: pay} = nodesA[i].pivots[j];
-                        var {x: pbx, y: pby} = nodesB[i].pivots[j];
-                        animNodes[i].pivots[j] = {x: pax+(pbx-pax)*t, y: pay+(pby-pay)*t};
-                    }
-                }
-            }
+            var animNodes = setupKeyframeForAnimation();
             for (var i = 0; i < animNodes.length; i++) {    
                 drawElements(context, animNodes[i]); 
             };
@@ -90,7 +112,6 @@ window.onload = function() {
         }
 
     }, 1000/30);
-
     
     // -- Rotation -- 
     canvas.onclick = function(e) {
@@ -110,7 +131,9 @@ window.onload = function() {
                 } 
             } 
         }
+        
     }
+
     // -- Drag and drop actions --
     canvas.onmousedown = function(e) {
         var positionClicked = [e.offsetX, e.offsetY];
@@ -161,11 +184,37 @@ window.onload = function() {
     }
 }
 
+function setupKeyframeForAnimation() {
+    var animKeyframe = (currentKeyframe + ((Date.now() - animate) / 1000)) % keyframes.length;
+    var nodesA = keyframes[Math.floor(animKeyframe)];
+    var nodesB = keyframes[Math.floor(animKeyframe + 1) % keyframes.length];
+    var animNodes = copyNodes(nodesA);
+
+    // There was no reduction or duplication
+    if (nodesA.length === nodesB.length) {
+        // updates position, angle and pivots of a node
+        for (var i = 0; i < animNodes.length; ++i) {
+            var t = animKeyframe % 1;
+            var {x: ax, y: ay} = nodesA[i].position;
+            var {x: bx, y: by} = nodesB[i].position;
+            var aa = nodesA[i].angle;
+            var ba = nodesB[i].angle;
+            animNodes[i].position = {x: ax+(bx-ax)*t, y: ay+(by-ay)*t};
+            animNodes[i].angle = aa + (ba - aa) * t;
+
+            for (var j = 0; j < 3; j++) {
+                var {x: pax, y: pay} = nodesA[i].pivots[j];
+                var {x: pbx, y: pby} = nodesB[i].pivots[j];
+                animNodes[i].pivots[j] = {x: pax+(pbx-pax)*t, y: pay+(pby-pay)*t};
+            }
+        }
+    }
+    return animNodes;
+}
+
+
 // --- Keyboard actions --- 
 window.addEventListener("keydown", keysPressed, false);
-
-var ctrlPressed = false;
-var hidePivots = false;
 
 function keysPressed(e) {
     var key = e.keyCode;
@@ -192,10 +241,8 @@ function keysPressed(e) {
         case (93):
             ctrlPressed = true;
         break;
-        case (32): // space bar: 
-            // add a new keyframe, registering all the data associated with the nodes
+        case (32): // space bar
             keyframes.push(copyNodes(nodes));
-            console.log("adding keyframe");
         break;
         case (80): // letter p
             // plays an animation to change keyframes 
@@ -215,15 +262,13 @@ function keysPressed(e) {
                 nodes = keyframes[keyframes.length - 1];
             }          
         break;
-
-
     }
 }
 
 function keysReleased(e) {
     ctrlPressed = false
 }
-
+// Makes a copy of nodes values
 function copyNodes(nodes) {
     var copy = [];
     // Create a copy of nodes
@@ -245,7 +290,6 @@ function copyNodes(nodes) {
                 var connectingOnPort = nodes[i].ports[0][1];
                 copy[i].ports[0] = [nodeForPort0, connectingOnPort];
             }
-            
             // Set infos for port 1
             if (nodes[i].ports[1][0].id === copy[j].id) {
                 var nodeForPort1 = copy[j];
@@ -280,7 +324,6 @@ function increaseKeyframe() {
 }
 
 
-
 // -- Transformation -- 
 /*
     Evaluate if the node clicked can do a transformation. There are 2 types:
@@ -289,7 +332,7 @@ function increaseKeyframe() {
 */
 function checkTransformation(node) {
     var pairToTransform = node.ports[0][0]; // get the node that the current node is connecting on port 0
-    
+        
     if (pairToTransform.ports[0][0] === node && // check if the other node on port 0 is equal to the current node
         (node !== nodes[0] && pairToTransform !== nodes[0])) { // initial node don't reduce
         if (node.label === pairToTransform.label) { // reduction
@@ -297,6 +340,9 @@ function checkTransformation(node) {
         } else { // duplication
             duplicateNodes(node, pairToTransform);
         }
+        // remove the current nodes from the array of nodes
+        nodes = nodes.filter(aux_node => (aux_node !== node && aux_node !== pairToTransform));
+        setInitialPositionForPivots(nodes);
     }
 }
 
@@ -313,12 +359,10 @@ function reduceNodes(nodeA, nodeB) {
         var b_destPort = nodeB.ports[i][1];
         connectPorts([nodeA_port_dest, a_destPort], [nodeB_port_dest, b_destPort]);
     }
-    // remove the reduced nodes from the array of nodes
-    nodes = nodes.filter(node => (node !== nodeA && node !== nodeB)); 
 }
 
-// -- Duplication --
-// Occurs between nodes with different labels. The nodes pass through each-other, duplicating themselves
+// // -- Duplication --
+// // Occurs between nodes with different labels. The nodes pass through each-other, duplicating themselves
 function duplicateNodes(nodeA, nodeB) {
     var xPositionLeft = nodeA.position.x - (nodeA.radius * 1.2);
     var xPositionRight = nodeA.position.x + (nodeA.radius * 1.2);
@@ -351,57 +395,6 @@ function duplicateNodes(nodeA, nodeB) {
 
     connectPorts([nodeB_leftDown, 0],[nodeA.ports[2][0], nodeA.ports[2][1]]);
     connectPorts([nodeB_rightDown, 0],[nodeA.ports[1][0], nodeA.ports[1][1]]);
-
-    // remove the reduced nodes from the array of nodes
-    nodes = nodes.filter(node => (node !== nodeA && node !== nodeB)); 
-    setInitialPositionForPivots(nodes);
-}
-
-
-// Defines the properties of each node
-function makeNodes() {
-    var nodes = [];
-
-    var initialNode = new Node(0, {x: width * 0.47 - 5, y: height * 0.05}, getRadianFromAngle());
-    nodes.push(initialNode);
-
-    var node0 = new Node(1, {x: width * 0.5, y: height * 0.2}, getRadianFromAngle(90));
-    nodes.push(node0);
-
-
-    var node1 = new Node(1, {x: width * 0.3, y: height * 0.40}, getRadianFromAngle());
-    nodes.push(node1);
-
-    var node2 = new Node(2, {x: width * 0.20, y: height * 0.60}, getRadianFromAngle());
-    nodes.push(node2);
-
-    // -10 is to align an upside down node with the others
-    var node3 = new Node(1, {x: width * 0.40, y: height * 0.60 - 10}, getRadianFromAngle(90));
-    nodes.push(node3); 
-
-
-    var node4 = new Node(1, {x: width - (width * 0.3), y: height * 0.40}, getRadianFromAngle());
-    nodes.push(node4);
-
-    for (var i = 0; i < nodes.length; i++) {
-        nodes[i].id = nodeIdCounter++;
-    }
-
-    // Connections between ports
-    connectPorts([node0, 0], [node1, 0]);
-    connectPorts([node0, 1], [node4, 0]);
-    connectPorts([node0, 2],[initialNode, 0]);
-    connectPorts([initialNode, 1], [initialNode, 2]);
-
-    connectPorts([node1, 1], [node2, 0]);
-    connectPorts([node1, 2], [node3, 2]);
-
-    connectPorts([node2, 1], [node3, 0]);
-    connectPorts([node2, 2], [node3, 1]);
-
-    connectPorts([node4, 1], [node4, 2]);
-
-    return nodes;
 }
 
 
@@ -451,16 +444,6 @@ function updatePivotsPosition(node) {
 // ----- Drawing ------
 // Draw the initial node as a small circle
 function drawInitialNode(context, node) {
-    if (elementSelected) {
-        if (elementSelected[0] === "initialNode") {
-            context.fillStyle = selectionColor;
-            context.strokeStyle = selectionColor;
-        } else {
-            context.fillStyle = 'black';
-            context.strokeStyle = 'black';
-        }
-    }
-    var port0Position = node.getPortPosition(0);
     context.beginPath();
     // context.arc(port0Position.x, port0Position.y, 5, 0, 2 * Math.PI);
     context.arc(node.position.x, node.position.y, 5, 0, 2 * Math.PI);
@@ -482,13 +465,17 @@ function drawElements(context, node) {
     
     if (elementSelected) {
         // Highlight the selected element
-        if (elementSelected[1] === node && elementSelected[0] === "node") {
+        if (elementSelected[1] === node && 
+            (elementSelected[0] === "node" || elementSelected[0] === "initialNode")) {
             context.strokeStyle = selectionColor; 
-        }
+            context.fillStyle = selectionColor;
+        }           
     }
+    
     if (elementClicked) {
         if (elementClicked === node) {
-            context.strokeStyle = selectionColor; 
+            context.strokeStyle = selectionColor;
+            context.fillStyle = selectionColor; 
         }
     }
 
